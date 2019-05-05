1f5cb305e076bb45c8f6ba3cd766032e48e95240
ZhangDubhe <zht_dubhe@163.com>
2019-05-05 23:36:24 +0800

    清理 about 和 contact 代码

--- src/views/About.vue
+++ src/views/About.vue
@@ -54,123 +54,37 @@ export default {
   }
 }
 </script>
-<style>
-.main .container-fluid {
-  padding: 0;
-}
-.home-page {
-  height: calc(100vh - 66px);
-  overflow: auto;
-}
-.home-page .container {
-  min-height: 100vh;
-  padding-bottom: 2rem;
-}
-/* .home-page .container {
-} */
-.home-page .flud {
-  max-width: calc(100% - 132px);
-  margin-top: 118px;
-  margin-bottom: 118px;
-  min-height: calc(100vh - 196px);
-}
-.home-page .grid-item,
-.home-page .grid-sizer {
-  width: calc(50% - 18px); 
-}
-.home-page .grid-item img {
-  width: 100%;
-}
-.home-page .project-item {
-  margin-bottom: 36px;
-}
-@media (max-width: 991px) {
-  .home-page .flud {
-    max-width: calc(100% - 40px);
-    padding-left: 0;
-    padding-right: 0;
-    margin-top: 48px;
-    padding-bottom: 20px;
-    margin-bottom: 30px;
-  }
-}
-@media (max-width: 768px) {
-  .home-page .flud {
-    max-width: 100%;
-    padding-left: 0;
-    padding-right: 0;
-    padding-bottom: 20px;
-    margin-bottom: 30px;
-  }
-  .home-page .grid-item,
-  .home-page .grid-sizer {
-    width: 100%;
-  }
-  .home-page .project-item {
-    margin-bottom: 22px;
-  }
-}
-</style>
 <style lang="scss">
-.home-page .project-item {
-  position: relative;
-  overflow: hidden;
-  .hover-show {
-    position: absolute;
-    top: 100%;
-    color: #ffffff;
-    background: rgba(115, 115, 115, 0.61);
-    transition: all 300ms cubic-bezier(0.23, 0.06, 0, 1.34) 100ms;
-    width: 100%;
-    height: 100%;
-    padding: 10px 14px;
-
-    display: flex;
-    overflow: hidden;
-    flex-flow: column nowrap;
-    justify-content: flex-end;
-    align-items: flex-start;
-
-    .item-title {
-      font-family: 'Mada SemiBold'
+  @media (max-width: 991px) {
+    .hover-show {
+      font-size: 0.8rem;
+    }
+    .ab-ctn{
+      margin-top:70px;
     }
   }
-
-  &:hover .hover-show {
-    // display: block;
-    top: 0;
+  @media(min-width:992px){
+    .ab-ctn{
+      margin-left: 155px;
+      margin-top: 70px;
+    }
   }
-}
-@media (max-width: 991px) {
-  .hover-show {
-    font-size: 0.8rem;
-  }
-  .ab-ctn{
-    margin-top:70px;
-  }
-}
-@media(min-width:992px){
-  .ab-ctn{
-    margin-left: 155px;
-    margin-top: 70px;
-}
-}
 </style>
 <style>
-.ab-title{
-  margin-left: 38px;
-  margin-top: 20px;
-}
-.about-page .row {
-  margin-left: 0;
-  margin-right: 0;
-}
-.ab-time {
-  width: 120px;
-}
-.about-page .ab-time,
-.about-page .ab-exp {
-  padding-left: 0 !important;
-  padding-right: 0 !important;
-}
+  .ab-title{
+    margin-left: 38px;
+    margin-top: 20px;
+  }
+  .about-page .row {
+    margin-left: 0;
+    margin-right: 0;
+  }
+  .ab-time {
+    width: 120px;
+  }
+  .about-page .ab-time,
+  .about-page .ab-exp {
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+  }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="home-page" ref="page">
+<div class="contact-page" ref="page">
   <section class="cnt-ctn">
     <ul>
       <li class="d-flex cnt"><strong>yiminminghuan@sina.com </strong></li>
@@ -38,93 +38,7 @@ export default {
   }
 }
 </script>
-<style>
-.main .container-fluid {
-  padding: 0;
-}
-.home-page {
-  height: calc(100vh - 66px);
-  overflow: auto;
-}
-.home-page .container {
-  min-height: 100vh;
-  padding-bottom: 2rem;
-}
-/* .home-page .container {
-} */
-.home-page .flud {
-  max-width: calc(100% - 132px);
-  margin-top: 118px;
-  margin-bottom: 118px;
-  min-height: calc(100vh - 196px);
-}
-.home-page .grid-item,
-.home-page .grid-sizer {
-  width: calc(50% - 18px); 
-}
-.home-page .grid-item img {
-  width: 100%;
-}
-.home-page .project-item {
-  margin-bottom: 36px;
-}
-@media (max-width: 991px) {
-  .home-page .flud {
-    max-width: calc(100% - 40px);
-    padding-left: 0;
-    padding-right: 0;
-    margin-top: 48px;
-    padding-bottom: 20px;
-    margin-bottom: 30px;
-  }
-}
-@media (max-width: 768px) {
-  .home-page .flud {
-    max-width: 100%;
-    padding-left: 0;
-    padding-right: 0;
-    padding-bottom: 20px;
-    margin-bottom: 30px;
-  }
-  .home-page .grid-item,
-  .home-page .grid-sizer {
-    width: 100%;
-  }
-  .home-page .project-item {
-    margin-bottom: 22px;
-  }
-}
-</style>
 <style lang="scss">
-.home-page .project-item {
-  position: relative;
-  overflow: hidden;
-  .hover-show {
-    position: absolute;
-    top: 100%;
-    color: #ffffff;
-    background: rgba(115, 115, 115, 0.61);
-    transition: all 300ms cubic-bezier(0.23, 0.06, 0, 1.34) 100ms;
-    width: 100%;
-    height: 100%;
-    padding: 10px 14px;
-
-    display: flex;
-    overflow: hidden;
-    flex-flow: column nowrap;
-    justify-content: flex-end;
-    align-items: flex-start;
-
-    .item-title {
-      font-family: 'Mada SemiBold'
-    }
-  }
-
-  &:hover .hover-show {
-    // display: block;
-    top: 0;
-  }
-}
 @media (max-width: 991px) {
   .hover-show {
     font-size: 0.8rem;
